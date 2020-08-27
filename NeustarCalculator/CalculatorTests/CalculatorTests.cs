610c62d08f248b321d319853636be7e7b8fe36b9
Sergio Flores <sergiofvasquez@agritradenet.com>
2020-08-27 12:28:49 -0600

    Exercise completed

---
+++ NeustarCalculator/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+
+using NeustarCalculator;
+using System.IO;
+
+namespace CalculatorTests
+{
+    public class CalculationOperationsTests
+    {
+        [Fact]
+        public void CalculatorAddition()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(5, 2);
+            Assert.Equal(7, result);
+        }
+    }
+
+    public class CalculatorConsoleIOTests
+    {
+        [Fact]
+        public void ConsoleValidation()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                //Program program = new Program();
+                Program.Main(new string[] {"7","2"});
+
+                string expected = "Please type the first number and press enter\n";
+                expected += "Please type the second number and press enter\n";
+                expected += "The result is 9\n";
+            }
+        }
+
+        [Fact]
+        public void StructureValidation()
+        {
+            //This test assures that the calculation is done specifically by the Calculator class and the result log is done specifically by the Logger class
+            Assert.Equal("Calculator", Program.Calculate().GetType().Name);
+            Assert.Equal("Logger", Program.LogResult().GetType().Name);
+        }
+    }
+}
---
+++ NeustarCalculator/NeustarCalculator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace NeustarCalculator
+{
+	public class Calculator
+	{
+		public int Add(int a, int b) { return a + b; }
+	}
+
+	public class Logger
+	{
+		public void Log(string message) { Console.WriteLine(message); }
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Please type the first number and press enter");
+			var firstNumber = 0;
+			var firstNumberString = GetFirstNumberString(args);
+			if (!Int32.TryParse(firstNumberString, out firstNumber))
+			{
+				FailedParsedStringHandling();
+				return;
+			}
+			Console.WriteLine("Please type the second number and press enter");
+			var secondNumber = 0;
+			var secondNumberString = GetSecondNumberString(args);
+			if (!Int32.TryParse(secondNumberString, out secondNumber))
+			{
+				FailedParsedStringHandling();
+				return;
+			}
+
+			var calculator = Calculate();
+			var logger = LogResult();
+			logger.Log("The result is " + calculator.Add(firstNumber, secondNumber).ToString());
+		}
+
+		public static Calculator Calculate()
+        {
+			return new Calculator();
+		}
+
+		public static Logger LogResult()
+        {
+			return new Logger();
+        }
+
+		private static void FailedParsedStringHandling()
+        {
+			Console.WriteLine("The number is invalid");
+        }
+
+		private static string GetFirstNumberString(string[] args)
+        {
+			if(args != null && args.Length > 0)
+            {
+				return args[0];
+            }
+			else
+            {
+				return Console.ReadLine();
+            }
+        }
+
+		private static string GetSecondNumberString(string[] args)
+		{
+			if (args != null && args.Length > 1)
+			{
+				return args[1];
+			}
+			else
+			{
+				return Console.ReadLine();
+			}
+		}
+	}
+}
